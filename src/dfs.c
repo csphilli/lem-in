d2d34cb459b06737b2f750b98410f18267667ee2
Christopher <csphilli@gmail.com>
2020-10-13 08:23:48 +0300

    more work on dfs. trying to solve memleak

--- src/dfs.c
+++ src/dfs.c
@@ -6,46 +6,47 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 15:11:52 by cphillip          #+#    #+#             */
-/*   Updated: 2020/10/12 16:53:44 by cphillip         ###   ########.fr       */
+/*   Updated: 2020/10/13 08:23:02 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-void	print_routes(t_entry **q)
+void	print_routes(t_routes *routes)
 {
 	t_entry **tmp;
 	int i;
 
 	i = 0;
-	tmp = q;
+	tmp = routes->q;
+	ft_printf("Crawled: ");
 	while (tmp[i])
 	{
-		ft_printf("%s", tmp[i]->name);
-		if (tmp[i + 1])
-			ft_printf(",");
+		ft_printf("%s | %p\n", tmp[i]->name, tmp[i]);
+		// if (tmp[i + 1])
+		// 	ft_printf(",");
 		i++;
 	}
-	ft_printf("\n");
+	// ft_printf("\n");
 }
 
 void	crawl(t_entry *entry, t_routes *routes)
 {
 	int	n_paths;
 	int	i;
-
-	n_paths = link_array_len(entry->link_arr);
+	
 	i = 0;
 	entry->visited = true;
 	if (!routes->q)
 	{
 		if (!(routes->q = (t_entry**)malloc(sizeof(t_entry*) * 2)))
 			exit_malloc();
-		routes->q[0] = entry; // leak here. taking entry away from 
+		routes->q[0] = entry;
 		routes->q[1] = NULL;	
 	}
 	else if (!link_exists(routes->q, entry))
 		routes->q = append_link(routes->q, entry);
+	n_paths = link_array_len(entry->link_arr);
 	while (i < n_paths)
 	{
 		if (!entry->link_arr[i]->visited)
@@ -70,13 +71,18 @@ void	do_dfs(t_bucket **ht, t_master *master)
 	n_paths = link_array_len(end->link_arr);
 	if (!(routes = (t_routes*)malloc(sizeof(t_routes))))
 		exit_malloc();
-	if (!(routes->q = (t_entry**)malloc(sizeof(t_entry*))))
-		exit_malloc();
-	routes->q[0] = NULL;
+	// routes = NULL;
+	// if (!(routes->q = (t_entry**)malloc(sizeof(t_entry*))))
+	// 	exit_malloc();
+	// routes->q[0] = NULL;
 	crawl(end, routes);
 	// ft_printf("MASTER: \n");
 	// ft_printf("S: %p | E: %p\n", master->start_room, master->end_room);
 	check_path_exists(start, end, routes);
+	print_routes(routes);
+	// while (1)
+	// {
+		
+	// }
 	
-	print_routes(routes->q);
 }
